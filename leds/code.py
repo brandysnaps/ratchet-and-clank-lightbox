d996723c21ed1871a36d005a0847d7f85d9d38d9
brandysnaps <brandonfarrell3004@gmail.com>
2022-09-30 11:59:26 +1300

    Add more animations

--- leds/code.py
+++ leds/code.py
@@ -13,10 +13,13 @@ from adafruit_led_animation.animation.colorcycle import ColorCycle
 from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.animation.pulse import Pulse
 from adafruit_led_animation.animation.solid import Solid
+from adafruit_led_animation.animation.sparkle import Sparkle
+from adafruit_led_animation.animation.SparklePulse import SparklePulse
 from adafruit_led_animation.color import CYAN
 
-# Define custom colors here
-CUSTOM_PINK = (216, 0, 166)
+# Define colors here
+COLOR_1 = CYAN
+COLOR_2 = (216, 0, 166)  # Pink
 
 # LED attributes
 pixel_brightness = 0.5
@@ -28,26 +31,38 @@ pixels = neopixel.NeoPixel(
 )
 
 # Animations
-solid_1 = Solid(pixels, color=CYAN)
-solid_2 = Solid(pixels, color=CUSTOM_PINK)
-color_cycle = ColorCycle(pixels, speed=5.0, colors=[CYAN, CUSTOM_PINK])
-chase_1 = Chase(pixels, speed=0.1, color=CUSTOM_PINK, size=2, spacing=3)
-chase_2 = Chase(pixels, speed=0.1, color=CYAN, size=2, spacing=3)
-comet_1 = Comet(pixels, speed=0.075, color=CUSTOM_PINK, bounce=False)
-comet_2 = Comet(pixels, speed=0.075, color=CYAN, bounce=False)
-pulse_1 = Pulse(pixels, speed=0.1, color=CYAN, period=5)
-pulse_2 = Pulse(pixels, speed=0.1, color=CUSTOM_PINK, period=5)
+chase_1 = Chase(pixels, speed=0.1, color=COLOR_2, size=2, spacing=3)
+chase_2 = Chase(pixels, speed=0.1, color=COLOR_1, size=2, spacing=3)
+color_cycle = ColorCycle(pixels, speed=5.0, colors=[COLOR_1, COLOR_2])
+comet_1 = Comet(pixels, speed=0.075, color=COLOR_2, bounce=False)
+comet_2 = Comet(pixels, speed=0.075, color=COLOR_1, bounce=False)
+pulse_1 = Pulse(pixels, speed=0.1, color=COLOR_1, period=5)
+pulse_2 = Pulse(pixels, speed=0.1, color=COLOR_2, period=5)
+solid_1 = Solid(pixels, color=COLOR_1)
+solid_2 = Solid(pixels, color=COLOR_2)
+sparkle_1 = Sparkle(pixels, speed=0.05, color=COLOR_1)
+sparkle_2 = Sparkle(pixels, speed=0.05, color=COLOR_2)
+sparkle_pulse_1 = SparklePulse(
+    pixels, speed=0.05, period=5, min_intensity=0, max_intensity=1.0, color=COLOR_1
+)
+sparkle_pulse_2 = SparklePulse(
+    pixels, speed=0.05, period=5, min_intensity=0, max_intensity=1.0, color=COLOR_2
+)
 
 animations = AnimationSequence(
-    solid_1,
-    solid_2,
-    color_cycle,
     chase_1,
     chase_2,
+    color_cycle,
     comet_1,
     comet_2,
     pulse_1,
     pulse_2,
+    solid_1,
+    solid_2,
+    sparkle_1,
+    sparkle_2,
+    sparkle_pulse_1,
+    sparkle_pulse_2,
     advance_interval=60,
     auto_clear=True,
     random_order=True,
